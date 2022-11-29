8fa329b477716b6da248d46b75dac1be098c6d87
wan-maoyuan <wanmaoyuan@outlook.com>
2022-11-29 12:01:08 +0800

    add config.ini

--- 标准地址/pg.py
+++ 标准地址/pg.py
@@ -1,14 +1,31 @@
 import psycopg2
+import os
+import configparser
+
+
+class ConnInfo:
+    def __init__(self):
+        pwd = os.path.split(os.path.realpath(__file__))[0]
+        configPath = os.path.join(pwd, "config.ini")
+
+        conf = configparser.ConfigParser()
+        conf.read(configPath)
+        self.dbname = conf.get("postgres", "dbname")
+        self.user = conf.get("postgres", "user")
+        self.password = conf.get("postgres", "password")
+        self.host = conf.get("postgres", "host")
+        self.port = conf.get("postgres", "port")
 
 
 class PgDB:
     def __init__(self):
+        config = ConnInfo()
         self.conn = psycopg2.connect(
-            dbname="geo_info",
-            user="root",
-            password="root",
-            host="1.14.103.216",
-            port="8432",
+            dbname=config.dbname,
+            user=config.user,
+            password=config.password,
+            host=config.host,
+            port=config.port,
         )
 
     def query_all(self):
